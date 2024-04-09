44cbf688092143c1a6752ac90b0364ce14868afc
Raymond <58218638+raybags-web-dev@users.noreply.github.com>
2024-04-09 16:53:11 +0200

    modularized configurations - set up flow and implimented settings from one source.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from src.utils.handle_writes import data_processor_pipeline
 from src.middleware import error_handler
 from src.middleware.warnings import suppress_warnings
+from ochestrator.ochestrator import load_configs
 from src.cloud.databricks import upload_files_to_databricks
+
+configs = load_configs()
 
 
 @error_handler.handle_error
@@ -9,9 +12,10 @@ from src.cloud.databricks import upload_files_to_databricks
 def main():
     # data processor pipeline
     is_data_ready = data_processor_pipeline()
+    upload_to_dbfs = configs['upload_to_dbfs']
 
     # upload data pipeline
-    if is_data_ready:
+    if is_data_ready and upload_to_dbfs:
         upload_files_to_databricks(is_data_ready)
 
 
